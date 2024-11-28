784a66bb9b7907f43721b58de1412206667bc756
ngoc-dan <ngocdan191026@gmail.com>
2024-11-28 22:12:55 +0700

    Add files via upload

--- 8.6.cpp
+++ 8.6.cpp
@@ -1,17 +1,16 @@
 #include<stdio.h>
 int main(){
-int i,j,sum=0;
+int i,sum=0;
 int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
 for( i=0 ; i<=2 ; i++){
-	printf("\n");
-    	for( j=0 ; j<=2 ; j++){
-    		if(i!=1 && j!=1 || i==1&&j==1){
-    			printf("%d ",arr[i][j]);
-    			sum=sum+arr[i][j];
+	
+    	for( i=0 ; i<=2 ; i++){
+    			printf("%d \n",arr[i][i]);
+    			sum=sum+arr[i][i];
     			
 			}
     	}
-    }
+    
     printf("\n");
     printf("tong la : %d",sum);
     return 0;
